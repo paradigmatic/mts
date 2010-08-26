88510ca0d91d5f04c73b69ad9cbab67469f98417
JL Falcone <jean-luc.falcone@unige.ch>
2010-08-26 08:58:16 +0200

    Introduced vector*scalar multiplication. Needs to be optimized and fully tested

--- src/vector/Vectors.scala
+++ src/vector/Vectors.scala
@@ -2,32 +2,36 @@ package ch.unige.mts.vector
 
 import no.uib.cipr.matrix.{ Vector => JVector }
 
+import Vector._
 
 trait Vector {
-
   def +[W <% Vector]( v: W ): Vector
+  def *( alpha: Double ): Vector
   def mtjVector(): JVector
   def apply( index: Int ) = mtjVector.get( index )
-
 }
 
 trait ModifiableVector extends Vector {
-  def update( index: Int, value: Double ) = mtjVector.set( index, value )
+  def update( index: Int, value: Double ): Unit
 }
 
 class VectorWrapper( val mtjVector: JVector ) extends ModifiableVector {
-
+  def *( alpha: Double ) = new VectorProxy( new ScaledVector( alpha, this ) )
   def +[W <% Vector]( v: W ) = 
     new VectorProxy( new VectorAdder( List( this, v ) ) )
+  def update( index: Int, value: Double ) { mtjVector.set( index, value ) }
 
 }
 
 class VectorProxy( private var delegate: Vector ) extends ModifiableVector {
-
+  def *( alpha: Double ) = {
+    delegate = delegate * alpha
+    delegate
+  }
   def +[W <% Vector]( v: W ) = delegate + v
   def mtjVector() = delegate.mtjVector
   override def apply( index: Int ) = delegate(index)
-  override def update( index: Int, value: Double ) = {
+  override def update( index: Int, value: Double ) {
     delegate match {
       case m: ModifiableVector => m(index) = value
       case _ => {
@@ -42,8 +46,8 @@ class VectorProxy( private var delegate: Vector ) extends ModifiableVector {
 }
 
 class VectorAdder( val vectors: List[Vector] )  extends Vector {
-
-  lazy val mtjVector = {
+ 
+  def mtjVector = {
     val v = vectors.head.mtjVector.copy
     for( w <- vectors.tail ) { v add w.mtjVector }
     v
@@ -52,11 +56,28 @@ class VectorAdder( val vectors: List[Vector] )  extends Vector {
   def +[W <% Vector]( v: W ) = 
     new VectorAdder( v :: vectors )
 
+  def *( alpha: Double ) = new VectorProxy( new ScaledVector( alpha, mtjVector ) ) 
+
 } 
+
+class ScaledVector( private var beta: Double, v: Vector ) extends Vector {
+  
+  def mtjVector() = v.mtjVector.copy scale beta
+
+  //TODO: Remove copy
+  def +[W <% Vector]( v: W ) = mtjVector.add( beta, v.mtjVector)
+    
+  def *( alpha: Double ) = {
+    beta *= alpha
+    this
+  }
+
+
+}
 
 object Vector {
 
-  implicit def mts2mtj( v: Vector ) = v.mtjVector
-  implicit def mtj2mts( v: JVector ) = new VectorWrapper( v )
+  implicit def mts2mtj( v: Vector ):JVector = v.mtjVector
+  implicit def mtj2mts( v: JVector ):VectorWrapper = new VectorWrapper( v )
 
 }
